c91a8ffc58f0dd374bf5b770e64a2fc7597ba53c
sejun42 <sejun1324@gmail.com>
2021-12-12 00:08:24 +0900

    Fixing the spaces

--- project/mainPage.py
+++ project/mainPage.py
@@ -1,37 +1,34 @@
-       class MyApp(QWidget):
+# 창 띄우기
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, qApp
+from PyQt5.QtCore import QCoreApplication
+from PyQt5.QtGui import QIcon
 
-            def __init__(self):
-                    super().__init__()
-                            self.initUI()
+class MyApp(QWidget):
 
-                                def initUI(self):
-                                        self.setWindowTitle('BSDP') # 창 타이틀
-                                                self.move(400, 120) # 창 위치
-                                                        self.resize(1200, 800) # 창 사이즈
+    def __init__(self):
+        super().__init__()
+        self.initUI()
 
-                                                                #가입하기 버튼
-                                                                        btnSign = QPushButton('가입하기', self)
-                                                                                btnSign.move(1030, 720) # 버튼 위치
-                                                                                        btnSign.resize(btnSign.sizeHint())
+    def initUI(self):
+        self.setWindowTitle('BSDP') # 창 타이틀
+        self.move(400, 120) # 창 위치
+        self.resize(1200, 800) # 창 사이즈
 
-     btnSign.clicked.connect(QCoreApplication.instance().quit) # quit 내용 바꿔야함
+        #가입하기 버튼
+        btnSign = QPushButton('가입하기', self)
+        btnSign.move(1030, 720) # 버튼 위치
+        btnSign.resize(btnSign.sizeHint())
+        btnSign.clicked.connect(QCoreApplication.instance().quit) # quit 내용 바꿔야함
 
+        #로그인하기 버튼
+        btnLogin = QPushButton('로그인하기', self)
+        btnLogin.move(900, 720)  # 버튼 위치
+        btnLogin.resize(btnLogin.sizeHint())
+        btnLogin.clicked.connect(QCoreApplication.instance().quit)  # quit 내용 바꿔야함
+        self.show()
 
-             #로그인하기 버튼
-
-                     btnLogin = QPushButton('로그인하기', self)
-
-                             btnLogin.move(900, 720)  # 버튼 위치
-
-                                     btnLogin.resize(btnLogin.sizeHint())
-
-                                             btnLogin.clicked.connect(QCoreApplication.instance().quit)  # quit 내용 바꿔야함
-
-                                                     self.show()
-
-                                                                                                                                                if __name__ == '__main__':
-                                                                                                                                                    app = QApplication(sys.argv)
-@@@
-mainPage.py[+] [unix] (08:59 01/01/1970)                                            1,1 Top
--- INSERT --
-
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec_())
